2c4dbeba40d257063c0354841a34793804b65a59
alex <alex_samarkin@mail.ru>
2019-12-12 01:53:42 +0300

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyData myData = new MyData();
+            myData.AddN(200);
+
+            chart1.Series[0].Points.Clear();
+
+            for (int i = 0; i < myData.X.Count; i++ )
+            {
+                chart1.Series[0].Points.AddXY(myData.X[i], myData.Y[i]);
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp2/MyData.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    public class MyData
+    {
+        /// <summary>
+        /// Название серии
+        /// </summary>
+        public string Title { get; set; } = "Мои данные";
+        /// <summary>
+        /// данные по Х b Y
+        /// </summary>
+        public List<double> X { get; set; } = new List<double>();
+        public List<double> Y { get; set; } = new List<double>();
+
+        /// <summary>
+        /// добавить точку
+        /// </summary>
+        /// <param name="x">координата х</param>
+        /// <param name="y">координата у</param>
+        public void AddXY(double x, double y)
+        {
+            X.Add(x);
+            Y.Add(y);
+        }
+        /// <summary>
+        /// добавить несколько точек
+        /// </summary>
+        /// <param name="N"> количество точек</param>
+        public void AddN(int N)
+        {
+            //удаляем старые данные
+            X.Clear();
+            Y.Clear();
+            //генератор случайных чисел
+            Random rnd = new Random();
+
+            for (int i = 0; i < N; i++)
+            {
+                //создаем и добавляем данные
+                AddXY(i, rnd.NextDouble());
+            }
+        }
+    }
+}
